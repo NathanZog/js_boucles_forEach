2c30d98ed57e5fa767323c9a5a969c995cb7f234
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-10 09:29:15 +0200

    save exo1 boucles for Each

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// 1
+
+// let monTab = ["Mouna","Ilias El", "Agim", "Kevin", "Antoine", "Seifidin", "Chris", "Haroune", "Elvis", "Nasila", "Van Hoa", "Jean", "Stan", "Ilias D", "Nathan", "Adil", "Ali", "Yassine", "Lira"];
+
+// monTab.forEach(elem => {
+//     console.log(`Bonjour ${elem}`);
+// });
+
+// 2
+
+let monTab = ["Lundi", "Mardi", "Mercredi", "Jeudi","Vendredi"];
+
+monTab.forEach(elem => {
+    console.log(elem);
+});
